2ae7f599a21adb0ed1a0b983c5a6805b69b54dec
eugent87 <trokhin87@gmail.com>
2024-09-25 21:48:18 +0300

    Добавьте файлы проекта.

---
+++ Inventory store/login_form.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Inventory_store
+{
+    partial class login_form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login_form));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.name_magazine = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.user_name = new System.Windows.Forms.Label();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.LogIn = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.checkBoxPass = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.name_magazine);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(474, 131);
+            this.panel1.TabIndex = 0;
+            // 
+            // name_magazine
+            // 
+            this.name_magazine.AutoSize = true;
+            this.name_magazine.Location = new System.Drawing.Point(171, 67);
+            this.name_magazine.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.name_magazine.Name = "name_magazine";
+            this.name_magazine.Size = new System.Drawing.Size(188, 21);
+            this.name_magazine.TabIndex = 6;
+            this.name_magazine.Text = "Management system";
+            this.name_magazine.Click += new System.EventHandler(this.name_magazine_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // user_name
+            // 
+            this.user_name.AutoSize = true;
+            this.user_name.Location = new System.Drawing.Point(68, 248);
+            this.user_name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.user_name.Name = "user_name";
+            this.user_name.Size = new System.Drawing.Size(106, 21);
+            this.user_name.TabIndex = 0;
+            this.user_name.Text = "User Name:";
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(71, 295);
+            this.textBox_username.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(309, 28);
+            this.textBox_username.TabIndex = 1;
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(68, 355);
+            this.password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(93, 21);
+            this.password.TabIndex = 2;
+            this.password.Text = "Password:";
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(71, 403);
+            this.textBox_password.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(309, 28);
+            this.textBox_password.TabIndex = 3;
+            this.textBox_password.UseSystemPasswordChar = true;
+            // 
+            // LogIn
+            // 
+            this.LogIn.Location = new System.Drawing.Point(71, 490);
+            this.LogIn.Name = "LogIn";
+            this.LogIn.Size = new System.Drawing.Size(330, 41);
+            this.LogIn.TabIndex = 4;
+            this.LogIn.Text = "Login";
+            this.LogIn.UseVisualStyleBackColor = true;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_clear.Location = new System.Drawing.Point(72, 444);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(89, 29);
+            this.btn_clear.TabIndex = 8;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // checkBoxPass
+            // 
+            this.checkBoxPass.AutoSize = true;
+            this.checkBoxPass.Location = new System.Drawing.Point(258, 448);
+            this.checkBoxPass.Name = "checkBoxPass";
+            this.checkBoxPass.Size = new System.Drawing.Size(158, 25);
+            this.checkBoxPass.TabIndex = 9;
+            this.checkBoxPass.Text = "Show Password";
+            this.checkBoxPass.UseVisualStyleBackColor = true;
+            this.checkBoxPass.CheckedChanged += new System.EventHandler(this.checkBoxPass_CheckedChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(435, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(39, 39);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // login_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(474, 667);
+            this.Controls.Add(this.checkBoxPass);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.LogIn);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.user_name);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "login_form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "login form";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label user_name;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button LogIn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label name_magazine;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.CheckBox checkBoxPass;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Inventory store/login_form.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_store
+{
+    public partial class login_form : Form
+    {
+        public login_form()
+        {
+            InitializeComponent();
+        }
+
+        private void name_magazine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBoxPass.Checked ) 
+            {
+                textBox_password.UseSystemPasswordChar = true;
+            }
+            else
+            {
+                textBox_password.UseSystemPasswordChar=false;
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            textBox_password.Clear();
+            textBox_username.Clear();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Exit Application","Confirm",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes) { Application.Exit(); }
+        }
+    }
+}
